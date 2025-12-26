a74317f07e0d323cde6a2b54f36aaefe5aaf0abb
han31023-sudo <han31023@gmail.com>
2025-12-26 10:34:49 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,10 @@
-const CACHE_NAME = 'my-pwa-cache-v1';
+const CACHE_NAME = "pwa-cache-v1";
 const FILES_TO_CACHE = [
-  './',
-  './index.html',
-  './style.css',   // kalau kamu punya
-  './app.js'       // kalau kamu punya
+  "./",
+  "./index.html"
 ];
 
-// event install — simpan file
-self.addEventListener('install', event => {
+self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       return cache.addAll(FILES_TO_CACHE);
@@ -15,11 +12,10 @@ self.addEventListener('install', event => {
   );
 });
 
-// event fetch — pakai cache dulu, kalau tidak ada, pakai network
-self.addEventListener('fetch', event => {
+self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(resp => {
-      return resp || fetch(event.request);
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
     })
   );
 });
